31dd22d3ac4de335bec68787462fd5b00e29b008
Mather <misaka@misaka.im>
2016-09-28 10:00:58 +0800

    + copy file bat.

---
+++ INC_copy.bat
@@ -0,0 +1,12 @@
+XCOPY D:\phpStudy\WWW\jiushouguoji-Mobile\dist D:\GitCode\jiushouguoji-Mobile\dist\ /Y/E/I
+XCOPY D:\phpStudy\WWW\jiushouguoji-Mobile\src D:\GitCode\jiushouguoji-Mobile\src\ /Y/E/I
+XCOPY D:\phpStudy\WWW\jiushouguoji-Mobile\gulpfile.js D:\GitCode\jiushouguoji-Mobile\ /Y
+XCOPY D:\phpStudy\WWW\jiushouguoji-Mobile\package.json D:\GitCode\jiushouguoji-Mobile\ /Y
+XCOPY D:\phpStudy\WWW\jiushouguoji-Mobile\INC_copy.bat D:\GitCode\jiushouguoji-Mobile\ /Y
+
+
+XCOPY D:\phpStudy\WWW\jiushouguoji-Mobile\dist D:\我的文档\jiushouguoji-Mobile\dist\ /Y/E/I
+XCOPY D:\phpStudy\WWW\jiushouguoji-Mobile\src D:\我的文档\jiushouguoji-Mobile\src\ /Y/E/I
+XCOPY D:\phpStudy\WWW\jiushouguoji-Mobile\gulpfile.js D:\我的文档\jiushouguoji-Mobile\ /Y
+XCOPY D:\phpStudy\WWW\jiushouguoji-Mobile\package.json D:\我的文档\jiushouguoji-Mobile\ /Y
+XCOPY D:\phpStudy\WWW\jiushouguoji-Mobile\INC_copy.bat D:\我的文档\jiushouguoji-Mobile\ /Y
