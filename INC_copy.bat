569f6b310c93dc868a6c248e8719f0c8750439df
Mather <misaka@misaka.im>
2016-09-28 11:14:23 +0800

    * details page DOM test.

--- INC_copy.bat
+++ INC_copy.bat
@@ -3,8 +3,6 @@ XCOPY D:\phpStudy\WWW\jiushouguoji-Mobile\src D:\GitCode\jiushouguoji-Mobile\src
 XCOPY D:\phpStudy\WWW\jiushouguoji-Mobile\gulpfile.js D:\GitCode\jiushouguoji-Mobile\ /Y
 XCOPY D:\phpStudy\WWW\jiushouguoji-Mobile\package.json D:\GitCode\jiushouguoji-Mobile\ /Y
 XCOPY D:\phpStudy\WWW\jiushouguoji-Mobile\INC_copy.bat D:\GitCode\jiushouguoji-Mobile\ /Y
-
-
 XCOPY D:\phpStudy\WWW\jiushouguoji-Mobile\dist D:\我的文档\jiushouguoji-Mobile\dist\ /Y/E/I
 XCOPY D:\phpStudy\WWW\jiushouguoji-Mobile\src D:\我的文档\jiushouguoji-Mobile\src\ /Y/E/I
 XCOPY D:\phpStudy\WWW\jiushouguoji-Mobile\gulpfile.js D:\我的文档\jiushouguoji-Mobile\ /Y
