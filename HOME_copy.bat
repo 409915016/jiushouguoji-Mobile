3364830938fe3a33868d768a0938ed8b2e3ab780
Mather <409915016@qq.com>
2016-10-02 02:04:49 +0800

    * update copy file bat.

---
+++ HOME_copy.bat
@@ -0,0 +1,12 @@
+XCOPY E:\phpStudy\WWW\gulptest\dist D:\M\MD2\jiushouguoji-Mobile\dist\ /Y/E/I
+XCOPY E:\phpStudy\WWW\gulptest\src D:\M\MD2\jiushouguoji-Mobile\src\ /Y/E/I
+XCOPY E:\phpStudy\WWW\gulptest\gulpfile.js D:\M\MD2\jiushouguoji-Mobile\ /Y
+XCOPY E:\phpStudy\WWW\gulptest\package.json D:\M\MD2\jiushouguoji-Mobile\ /Y
+XCOPY E:\phpStudy\WWW\gulptest\HOME_copy.bat D:\M\MD2\jiushouguoji-Mobile\ /Y
+
+XCOPY E:\phpStudy\WWW\gulptest\dist D:\M\MD2\jiushouguoji-Mobile-github\dist\ /Y/E/I
+XCOPY E:\phpStudy\WWW\gulptest\src D:\M\MD2\jiushouguoji-Mobile-github\src\ /Y/E/I
+XCOPY E:\phpStudy\WWW\gulptest\gulpfile.js D:\M\MD2\jiushouguoji-Mobile-github\ /Y
+XCOPY E:\phpStudy\WWW\gulptest\package.json D:\M\MD2\jiushouguoji-Mobile-github\ /Y
+XCOPY E:\phpStudy\WWW\gulptest\HOME_copy.bat D:\M\MD2\jiushouguoji-Mobile-github\ /Y
+
