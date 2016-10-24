f4c2634f1d2febf5b32630ffe2d1b6933a2f5065
Mather <misaka@misaka.im>
2016-10-24 17:17:52 +0800

    + Add search page.

--- dist/js/details.js
+++
@@ -1,5 +0,0 @@
-$(function ($) {
-    var shop_car_count = $(".product-nav-bar ul li:nth-child(2)");
-    shop_car_count[0].dataset.shopCount = "0";
-
-})  
--- dist/js/main.js
+++ dist/js/main.js
@@ -232,6 +232,9 @@ $(document).ready(function () {
     timer();
 
 
+    
+
+
 
 
 
