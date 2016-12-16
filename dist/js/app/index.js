d60d6f7ff04d414c3338c4c1036ad1d71c123f24
Mather <409915016@qq.com>
2016-12-16 17:29:59 +0800

    + Add loading more product function.

--- dist/js/app/index.js
+++ dist/js/app/index.js
@@ -9,6 +9,18 @@ function search_wrapper_close() {
     })
 }
 
+
+
+//传入字符串，返回DOM
+function parseDom(arg) {
+    var objE = document.createElement("div");
+    objE.innerHTML = arg;
+    return objE.childNodes;
+
+};
+
+
+
 var search_wrapper = get(".product-search-wrapper").innerHTML;
 //点击假的搜索栏 弹出页面
 get(".fake-product-search").addEventListener("click", function () {
@@ -26,6 +38,8 @@ get(".fake-product-search").addEventListener("click", function () {
         //console.log(this.value);
     });
 })
+
+
 
 //加载后再设置文字大小
 $("body > div.wrapper > div.nav-bar > ul > li > a").css("font-size", "14px");
@@ -46,38 +60,125 @@ $(".search-bar-more").click(function () {
 $(".main-wrapper").click(function () {
     $(".search-bar-menu").hide();
 });
+//
 
 
+var offset = $('#offset');
+var docheight = $('#docheight');
+var winheight = $('#winheight');
+
+//进度条顶部偏移
+var scrollTop = $(this).scrollTop();
+//文档高度
+var scrollHeight = $(document).height();
+//当前窗口高度
+var windowHeight = $(this).height();
+
+var isPostP =  $('#isPostP');
+
+var countP =  $('#count');
+
+var isPostPx = 600;
+
+var isPost = true;
+
+var PageValue = 1;
+
+//每次加载的数量
+var OnesLodingNum = 6;
+//当前加载的位置
+var LodingNumFlag = 0;
+
+
+//本地获取到的商品数据
 var goods_recommend = [];
-$.getJSON('js/goods.json', function (data){
 
-    switch (data.status) {
+var Height_minus = ( $(document).height() - $(this).height() - $(this).scrollTop() );
 
-        case 1 : {
-            console.table(data.data);
-            var t = data.data;
-            //data.data each
-            $(t).each(function (i ,element) {
-                var i = {};
-                //var m = ['id', 'name','price','image'];
+function scrol_test() {
+    //偏移
+    offset.text($(this).scrollTop());
+    //文档长度
+    docheight.text($(document).height());
+    //窗口高度
+    winheight.text($(this).height());
+    Height_minus = ( $(document).height() - $(this).height() - $(this).scrollTop() );
+    countP.text(Height_minus);
+    if ( (Height_minus < isPostPx) && isPost ){
 
-                i.id = element.id;
-                i.name = element.name;
-                i.price = element.price;
-                i.image = element.image;
-                goods_recommend.push(i);
+        getData(PageValue, OnesLodingNum);
+        isPost = false;
+        isPostP.text("■");
+        var tepWrapper = $(".loading-more")[0];
+        //加入DOM文档
+        //从当前加载位置开始 到加载个数阀值停止
+        for (var i = 0; i < OnesLodingNum ; i++)
+        {
+            console.log(goods_recommend);
+            console.log(LodingNumFlag);
+            var goods_url = goods_recommend[i].url;
+            var goods_imgURL = goods_recommend[i].image;
+            var goods_name = goods_recommend[i].name;
+            var goods_price = goods_recommend[i].price;
 
-            })
-            console.table(goods_recommend);
+            var html_temp = '<div class="single-goods">' + '<a href="' + goods_url + '">' + '<div class="product-imgBox">' +
+                '<img src="' + goods_imgURL + '" alt=""> ' + '</div>' + '<div class="product-name-price">' +
+                '<p>' + goods_name + '</p>' + '<p>¥<span>' + goods_price + '</span></p>' + '</div>' + '</a>' + '</div>';
+
+            $( parseDom(html_temp)).appendTo(tepWrapper);
+
         }
+        isPost = true;
+    } else  {
+        isPostP.text("□");
     }
 
+}
 
-    if(data.status == 1) {
+function getData(p , n) {
+    var num = n;
+    var page = p;
+    var PostUrl = 'http://wap.jiushouguoji.com/rcm?page=' + page + '&n=' + num ;
+    $.getJSON(PostUrl, function (data){
+        switch (data.status) {
+            case 1 : {
+                //console.table(data.data);
+                var t = data.data;
+                //data.data each
+                //每次过来要清空数组
+                goods_recommend.splice(0,goods_recommend.length);
+                //console.table(goods_recommend);
+                $(t).each(function (i ,element) {
+                    var i = {};
+                    //var m = ['id', 'name','price','image'];
+                    i.url = element.url;
+                    i.name = element.name;
+                    i.price = element.price;
+                    i.image = element.image;
+                    goods_recommend.push(i);
+
+                })
+                //console.table(goods_recommend);
+                //成功获取一次后 PageValue 要增加
+
+                PageValue += 1;
+            }
+        }
+
+        if(data.status == 1) {
+        }
+    })
+}
+
+function loadingChecker() {
+    scrol_test();
+    setTimeout('loadingChecker()', 100)
+}
 
 
-    }
+$(document).ready(function(){
+    getData();
+    loadingChecker();
 
+});
 
-
-})
